cc1252461da410eb14376e6c9f684b46d0458bf2
Dmitrii Vladimirov <dvlad7909@gmail.com>
2023-12-04 18:42:38 -0800

    gists field verification

--- components/json_locators.py
+++ components/json_locators.py
@@ -2,4 +2,5 @@ class JsonLocators:
     file_name = "json_response.json"
     number_of_repos_json = "$.public_repos"
     number_of_followers_json = "$.followers"
-    number_of_following = "following"
+    number_of_following_json = "following"
+    number_of_gists_json = "public_gists"
